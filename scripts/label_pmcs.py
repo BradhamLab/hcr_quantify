269fdd8e8804471525db2ee574d363d3c159c756
dakota-hawkins <dakotahawkins0110@gmail.com>
2022-02-16 19:39:06 -0500

    updated label post-processing.

--- scripts/label_pmcs.py
+++ scripts/label_pmcs.py
@@ -1,6 +1,8 @@
 import h5py
 import numpy as np
-from scipy import signal
+from scipy import signal, spatial
+from scipy import ndimage as ndi
+from scipy.spatial.qhull import QhullError
 from skimage import filters, measure, morphology, segmentation
 
 
@@ -72,10 +74,13 @@ def assign_to_label(src, region, slc, new_label):
     region : skimage.measure.RegionProperties
         Region to label
     slc : slice
-        Slice defining which part of the region to label.
+        Slice defining which part of the region to label. If None, selects
+        entire region
     new_label : int
         New label to set
     """
+    if slc is None:
+        slc = tuple([slice(None)] * len(region.image.shape))
     src[region.slice][slc][region.image[slc]] = new_label
 
 
@@ -154,6 +159,122 @@ def filter_small_ends(labels, region, min_pixels=5):
         i -= 1
 
 
+def backpropogate_split_labels(z_stack, labels):
+    """Propogates split labels in a Z stack to lower slices.
+
+    Parameters
+    ----------
+    z_stack : numpy.ndarray
+        3D image stack from z=0 ... z=Z
+    labels : numpy.ndarray
+        Labels for current z-slice, should be z=Z slice.
+
+    Returns
+    -------
+    numpy.ndarray
+        Newly labelled `z_stack`
+    """
+    new_stack = np.zeros_like(z_stack, dtype=int)
+    new_stack[-1, :, :] = labels
+    for i in range(1, z_stack.shape[0])[::-1]:
+        new_stack[i - 1, :, :] = segmentation.watershed(
+            np.zeros_like(z_stack[i, :, :]),
+            markers=new_stack[i, :, :],
+            mask=z_stack[i - 1, :, :],
+        )
+    return new_stack
+
+
+def z_split_labels(region, n_labels):
+    """Split labels along the z-plane.
+
+    Splits label along the z-plane if labels in individual z-slices have
+    multiple connected components.
+
+    Parameters
+    ----------
+    region : skimage.measure.RegionProperty
+        Region to check and possibly split.
+    n_labels : int
+        Total number of current labels.
+
+    Returns
+    -------
+    tuple : (numpy.ndarray, int)
+        numpy.ndarray : Previous label split along z-axis if the label should be
+            split. Otherwise, an array of zeros.
+        int : Total number of current labels
+    """
+    new_labs = np.zeros_like(region.image, dtype=int)
+    expand_labels = False
+    n_new_labels = 0
+    n_split = 0
+    for i, z_slice in enumerate(region.image):
+        segmented = measure.label(z_slice)
+        new_labs[i, :, :] = segmented
+        z_regions = measure.regionprops(new_labs[i, :, :])
+        if len(z_regions) > 1 and not expand_labels:
+            n_split += 1
+            centroids = [r.centroid for r in z_regions]
+            if n_split > 1 or max(spatial.distance.pdist(centroids)) > 5:
+                expand_labels = True
+                n_new_labels = len(np.unique(segmented)) - 1  # -1 for zeros
+                if i != 0:
+                    new_labs[: (i + 1), :, :] = backpropogate_split_labels(
+                        new_labs[: (i + 1), :, :], new_labs[i, :, :]
+                    )
+
+        elif expand_labels and i > 0:
+            # fill in z labels with z-1 labels
+            z_filled = segmentation.watershed(
+                np.zeros_like(new_labs[i, :, :]),
+                markers=new_labs[i - 1, :, :],
+                mask=new_labs[i, :, :],
+            )
+            # check if all regions in z labels are accounted for, otherwise
+            # create new label
+            for r in z_regions:
+                if np.isin(0, z_filled[r.slice][r.image]):
+                    n_new_labels += 1
+                    z_filled[r.slice][r.image] = n_new_labels
+            new_labs[i, :, :] = z_filled
+    # re-assign label values to avoid conflict with previous labels + maintain
+    # consistency
+    for r in measure.regionprops(new_labs):
+        if r.label == 1:
+            assign_to_label(new_labs, r, None, region.label)
+        else:
+            assign_to_label(new_labs, r, None, n_labels + 1)
+
+            n_labels += 1
+    return new_labs, n_labels
+
+
+def filter_by_area_length_ratio(labels, region, min_ratio=15):
+    """Filter cylindrical-like labels from a 3D label image.
+
+    Filters thin, long regions by comparing the (Total Area) / (Z length) ratio.
+
+    Parameters
+    ----------
+    labels : numpy.ndarray
+        Original 3D label image containing labels to filter.
+    region : skimage.RegionProperty
+        Region to possibly filter
+    min_ratio : int, optional
+        Minumum area / z length ratio to keep labels By default 15, and any
+        label with a smaller ratio will be removed
+    """
+    if region.area / region.image.shape[0] < min_ratio:
+        assign_to_label(labels, region, None, 0)
+
+
+def renumber_labels(labels):
+    """Renumber labels to that N labels will be labelled from 1 ... N"""
+    for i, region in enumerate(measure.regionprops(labels)):
+        assign_to_label(labels, region, None, i + 1)
+
+
 def generate_labels(
     stain, pmc_probs, p_low=0.5, p_high=0.8, selem=None, max_stacks=7, min_stacks=3
 ):
@@ -195,30 +316,61 @@ def generate_labels(
         selem[2, 1:4, 1:4] = morphology.disk(1)
     pmc_seg = filters.apply_hysteresis_threshold(pmc_probs, p_low, p_high)
     seeds = measure.label(morphology.binary_opening(pmc_seg, selem=selem))
-    seeds = measure.label(seeds > 0)
-    gradients = filters.sobel(stain, axis=0)
+    try:
+        gradients = filters.sobel(stain, axis=0)
+    except TypeError:
+        gradients = np.array([filters.sobel(x) for x in stain])
     labels = segmentation.watershed(
         np.abs(gradients),
         seeds,
-        # mask=pmc_seg,
-        mask=morphology.binary_closing(pmc_seg),
+        mask=np.stack(
+            morphology.closing(
+                ndi.binary_fill_holes(x, morphology.disk(2)),
+                None,
+            )
+            for x in pmc_seg
+        ),
     )
     n_labels = len(np.unique(labels)) - 1
 
+    # close up any holes in labels
+    for region in measure.regionprops(labels):
+        filled = morphology.binary_closing(region.image)
+        labels[region.slice][filled] = region.label
+        # assign_to_label(labels, region, (slice(None), slice(None), slice(None)), region.label)
+
+    # check for abnormally large labels, possibly merged PMCS, so divide labels
+    # if they separate in the z-plane
+    for region in measure.regionprops(labels):
+        try:
+            region.feret_diameter_max
+        except QhullError:
+            continue
+        if region.feret_diameter_max > 20:
+            split, n_labels_new = z_split_labels(region, n_labels)
+            if n_labels != n_labels_new:
+                assign_to_label(labels, region, None, 0)
+                for each in np.unique(split):
+                    if each != 0:
+                        labels[region.slice][split == each] = each
+            n_labels = n_labels_new
+
+    # a check to ensure we're keeping track properly
+    assert n_labels == len(measure.regionprops(labels))
+
+    # find abnormally long tracks, check for local minima in area size that
+    # would indicate stacked pmcs. Additionally clean up small tails in labels.
     for region in measure.regionprops(labels):
         n_stacks = np.unique(region.coords[:, 0]).size
         if n_stacks > max_stacks:
             n_labels = split_labels_by_area(labels, region, n_labels)
         elif n_stacks < min_stacks:
-            assign_to_label(labels, region, (slice(None), slice(None), slice(None)), 0)
+            assign_to_label(labels, region, None, 0)
+        filter_by_area_length_ratio(labels, region, min_ratio=15)
         filter_small_ends(labels, region, min_pixels=5)
 
-    # we've now filtered labels, so re-analyze regions
-    for region in measure.regionprops(labels):
-        closed = morphology.closing(
-            region.image,
-        )
-        labels[region.slice][closed] = region.label
+    # renumber labels from 1...N
+    renumber_labels(labels)
 
     return labels
 
@@ -358,6 +510,14 @@ if __name__ == "__main__":
         pmc_probs = np.array(h5py.File(snakemake.input["probs"], "r")["exported_data"])[
             :, :, :, 1
         ]
-        pmc_stain = np.array(h5py.File(snakemake.input["norm_stain"], "r")["image"])
-        pmc_segmentation = find_pmcs(pmc_stain, pmc_probs)
-        labels_to_hdf5(pmc_segmentation, snakemake.output)
+        pmc_stain = np.array(h5py.File(snakemake.input["stain"], "r")["image"])
+        pmc_segmentation = generate_labels(
+            pmc_stain,
+            pmc_probs,
+            p_low=0.45,
+            p_high=0.5,
+            selem=None,
+            max_stacks=7,
+            min_stacks=3,
+        )
+        labels_to_hdf5(pmc_segmentation, snakemake.output["labels"])

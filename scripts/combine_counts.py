74f35cad8e24cb7ed7075e0443b632dbac31d96e
dakota-hawkins <dakotahawkins0110@gmail.com>
2022-04-13 11:25:36 -0400

    fixed concatenation.

--- scripts/combine_counts.py
+++ scripts/combine_counts.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        snakmake
+        snakemake
     except NameError:
         snakemake = None
     if snakemake is not None:
-        embryo_counts = pd.concat[pd.read_csv(x) for x in snakemake.input]
-        embryo_counts.to_csv(snakemake.output)
+        embryo_counts = pd.concat([pd.read_csv(x) for x in snakemake.input["counts"]])
+        print(embryo_counts.shape)
+        embryo_counts.to_csv(snakemake.output["csv"])
